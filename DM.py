8c724c0aba920ac6bee5f3fa8f0760eddb8e61d2
pseudoboss <pseudoboss11@gmail.com>
2015-10-14 12:29:28 +0000

    added catchall image for when weather falls through

--- DM.py
+++ DM.py
@@ -10,6 +10,7 @@ weather = weather.read()
 weather = weather.decode('utf-8')
 weather = json.loads(weather)
 icon = weather['current_observation']['icon']
+print (icon)
 
 imageLocation = os.path.abspath(os.path.dirname(__file__))+'/'
 fehcmd = 'feh --bg-fill '+imageLocation
@@ -17,15 +18,18 @@ partlycloudy = 'PartlyCloudy.png'
 rainy = 'Rainy.png'
 sunny = 'Sunny.png'
 cloudy = 'Cloudy.png'
+other = 'SexySexySergal.jpg'
 
-if icon == 'partlycloudy':
-	fehcmd += partlycloudy
-elif icon == 'sunny':
+if icon in ('sunny', 'clear'):
 	fehcmd += sunny
+elif icon == 'partlycloudy':
+	fehcmd += partlycloudy
 elif icon == 'cloudy':
 	fehcmd += cloudy
 elif icon == 'rainy':
 	fehcmd += rainy
+else:
+	fehcmd += other
 
 print(fehcmd)
 subprocess.call(fehcmd, shell=True)
